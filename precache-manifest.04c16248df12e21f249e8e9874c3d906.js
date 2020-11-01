697f3e1d2050df77e081c925514064f0069eeb0c
Himanshu Tariyal <tariyal2000@gmail.com>
2020-11-01 12:14:22 +0530

    Updates

---
+++ precache-manifest.04c16248df12e21f249e8e9874c3d906.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1ee537d832dd9c240ebdeb578d4e7c2",
+    "url": "/snake/index.html"
+  },
+  {
+    "revision": "457cf7444c5a4f833e8b",
+    "url": "/snake/static/css/main.e62d2657.chunk.css"
+  },
+  {
+    "revision": "435f2df12f88fac2d629",
+    "url": "/snake/static/js/2.f6b3d5c9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/snake/static/js/2.f6b3d5c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "457cf7444c5a4f833e8b",
+    "url": "/snake/static/js/main.cf5ae0bc.chunk.js"
+  },
+  {
+    "revision": "b0fa3a03649fed88e553",
+    "url": "/snake/static/js/runtime-main.a72edac6.js"
+  }
+]);
